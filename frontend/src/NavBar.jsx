f5e6b539ab862b1b912fa9de72233d6aca5d0afb
kiran685 <sskiran961@gmail.com>
2025-09-21 15:55:12 +0530

    kovafd

--- frontend/src/NavBar.jsx
+++ frontend/src/NavBar.jsx
@@ -4,7 +4,7 @@ const NavBar=()=>{
     return(
         <div className="flex justify-around h-16 items-center border-2 border-black w-full">
             <h1 className=" md:sm text-xl font-xl font-bold">BlogVerse</h1>
-            <Link to="/home"className="text-gray-600 hover:bg-blue-200 px-5 py-2 rounded-xl cursor-pointer">Home</Link>
+            <Link to="/"className="text-gray-600 hover:bg-blue-200 px-5 py-2 rounded-xl cursor-pointer">Home</Link>
         <div  className="flex gap-5">
             <Link to="/signin" className="font-semibold px-5 py-2 text-gray-600">Sign In</Link>
             <Link to="/signup" className=" bg-blue-600 text-white px-5 py-2 rounded-xl cursor-pointer">Sign Up</Link>
